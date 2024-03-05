c8f48f81579bb272fa1fbb0117c183e6f12f0299
Chad Cote <chadcote58@gmail.com>
2024-03-05 16:14:01 -0500

    created the button for likes to apprear when the user is not the author

--- src/components/posts/PostDetails.jsx
+++ src/components/posts/PostDetails.jsx
@@ -3,7 +3,7 @@ import { getAllPosts, getPostById } from "../../services/postService.jsx";
 import { useParams } from "react-router-dom";
 import "./PostDetails.css";
 
-export const PostDetailsView = () => {
+export const PostDetailsView = ({ currentUser }) => {
   const [post, setPost] = useState({});
   const { postId } = useParams();
 
@@ -30,7 +30,17 @@ export const PostDetailsView = () => {
       </section>
       <section className="date-likes">
         <div className="date">{post.date}</div>
-        <div className="post-likes">{post.likes?.length} Likes</div>
+        {/* i want the button to appear if the current user is not the author. other wise just show the amount of likes  */}
+        <div className="likes-container">
+          <div className="btn-container">
+            {currentUser.id != post.userId ? (
+              <button className="btn">Like</button>
+            ) : (
+              ""
+            )}
+          </div>
+          <div className="post-likes">{post.likes?.length} Likes</div>
+        </div>
       </section>
     </article>
   );
