a16f25632fad6f43e43c7b291575267fd417c625
Chad Cote <chadcote58@gmail.com>
2024-03-05 15:05:23 -0500

    added some styling to the post details view

--- src/components/posts/PostDetails.jsx
+++ src/components/posts/PostDetails.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getAllPosts, getPostById } from "../../services/postService.jsx";
 import { useParams } from "react-router-dom";
+import "./PostDetails.css";
 
 export const PostDetailsView = () => {
   const [post, setPost] = useState({});
@@ -20,16 +21,16 @@ export const PostDetailsView = () => {
   return (
     <article className="full-post">
       <section className="post-info">
-        <div className="post-title">{post.title}</div>
+        <div className="pst-details-title">{post.title}</div>
         <div className="post-author">{post.user?.fullName}</div>
-        <div className="post-topic">{post.topic?.name}</div>
+        <div className="post-details-topic">{post.topic?.name}</div>
       </section>
       <section className="post-body">
         <div className="body">{post.body}</div>
       </section>
       <section className="date-likes">
         <div className="date">{post.date}</div>
-        <div className="likes">{post.likes?.length}</div>
+        <div className="post-likes">{post.likes?.length} Likes</div>
       </section>
     </article>
   );
