201a7d5fff0be2c404161eb61c8b6ccccbace06f
Chad Cote <chadcote58@gmail.com>
2024-03-05 09:17:43 -0500

    created the routes for login and register. wrapped application views in authorized

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 import { Posts } from "./components/posts/Allposts.jsx";
 import "./App.css";
+import { Login } from "./components/auth/Login.jsx";
+import { Register } from "./components/auth/Register.jsx";
+import { Authorized } from "./views/Authorized.jsx";
+import { ApplicationViews } from "./views/ApplicationViews.jsx";
 
 export const App = () => {
   return (
@@ -7,7 +11,18 @@ export const App = () => {
       <div className="title">
         <h1>Welcome to Learning Moments!</h1>
       </div>
-      <Posts />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="*"
+          element={
+            <Authorized>
+              <ApplicationViews />
+            </Authorized>
+          }
+        />
+      </Routes>
     </>
   );
 };
