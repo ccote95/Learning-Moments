19a6cec5aab207d62670bb99c3748ff6f6d2263e
Chad Cote <chadcote58@gmail.com>
2024-03-06 09:12:32 -0500

    created the form for the new post page

--- src/components/posts/NewPost.jsx
+++ src/components/posts/NewPost.jsx
@@ -1,8 +1,32 @@
+import { useEffect, useState } from "react";
+import { GetAllTopics } from "../../services/TopicService.jsx";
+import { TopicDropDown } from "./Dropdown.jsx";
+
 export const NewPost = () => {
+  const [allTopics, setAllTopics] = useState([]);
+  useEffect(() => {
+    GetAllTopics().then((topicArray) => {
+      setAllTopics(topicArray);
+    });
+  }, []);
+
   return (
-    <div>
-      <form>
-        <input type="text" />
+    <div className="form-container">
+      <form className="form">
+        <div className="new-pst-title">
+          <input type="text" placeholder="Post Title" />
+        </div>
+        <div>
+          <select className="new-pst-topic">
+            <option>All Topics</option>
+            {allTopics.map((topic) => {
+              return <TopicDropDown topic={topic} key={topic.id} />;
+            })}
+          </select>
+        </div>
+        <div>
+          <input type="text" placeholder="type your post here" />
+        </div>
       </form>
     </div>
   );
