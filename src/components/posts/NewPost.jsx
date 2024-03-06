da79bd8011d4479bad6299f80ad225a574458455
Chad Cote <chadcote58@gmail.com>
2024-03-06 09:40:07 -0500

    added some css to the new post page

--- src/components/posts/NewPost.jsx
+++ src/components/posts/NewPost.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { GetAllTopics } from "../../services/TopicService.jsx";
 import { TopicDropDown } from "./Dropdown.jsx";
+import "./NewPost.css";
 
 export const NewPost = () => {
   const [allTopics, setAllTopics] = useState([]);
@@ -11,23 +12,30 @@ export const NewPost = () => {
   }, []);
 
   return (
-    <div className="form-container">
-      <form className="form">
-        <div className="new-pst-title">
-          <input type="text" placeholder="Post Title" />
-        </div>
-        <div>
-          <select className="new-pst-topic">
-            <option>All Topics</option>
-            {allTopics.map((topic) => {
-              return <TopicDropDown topic={topic} key={topic.id} />;
-            })}
-          </select>
-        </div>
-        <div>
-          <input type="text" placeholder="type your post here" />
-        </div>
-      </form>
+    <div className="container">
+      <div className="form-container">
+        <form className="form">
+          <div className="new-pst-title">
+            <input type="text" placeholder="Post Title" />
+          </div>
+          <div>
+            <select className="new-pst-topic">
+              <option>All Topics</option>
+              {allTopics.map((topic) => {
+                return <TopicDropDown topic={topic} key={topic.id} />;
+              })}
+            </select>
+          </div>
+          <div className="post-text">
+            <textarea
+              className="text"
+              type="text"
+              placeholder="type your post here"
+            ></textarea>
+            <button className="save-btn">Save</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
